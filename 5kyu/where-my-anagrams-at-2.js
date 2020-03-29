364a71bdcfc72916c15bf86868402ec5c6eef656
Scott <scott.bishop.dev@gmail.com>
2020-03-29 13:18:23 -0700

    Prep anagrams for v2

---
+++ 5kyu/where-my-anagrams-at-2.js
@@ -0,0 +1,8 @@
+// Solve it by adding a sort function to the string prototype and using only the array
+// methods of split, join and filter throughout the program.
+
+// Add a sort method to the string prototype so that you can say someString.sort()
+
+function anagrams(word, words) {
+  
+}
