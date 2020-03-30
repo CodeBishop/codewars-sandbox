0b405045ff561963aa6dce41747e2e5f0de3ba94
Scott <scott.bishop.dev@gmail.com>
2020-03-29 20:50:03 -0700

    Optimize tokenizer

--- 4kyu/simple-syntax-tokenizing.js
+++ 4kyu/simple-syntax-tokenizing.js
@@ -4,33 +4,42 @@
 // tokenise("A + B * C") === [ "A", "+", "B", "*", "C" ]
 // tokenise("function a(arg, arg)") === [ "function", "a", [ "arg", ",", "arg" ] ]
 
+const {describe, it, Test} = require('../test-framework')
+
+const identifierRegex = new RegExp("(^[a-zA-Z]+)")
+const operatorRegex = new RegExp("^([!#$%&*+\-\/<=>@^_.,;]+)")
+
 function tokenise(string) {
   // Test for paranthesis mismatch
   if(string.split('(').length !== string.split(')').length) {return null}
 
   const tokens = []
-  let state = UNINITIALIZED
   let matchData = '', i = 0, substr
 
   while (i < string.length) {
+    // Skip over whitespace
+    while(' \n\r\t'.includes(string[i]) && i < string.length) {i++}
+    // Extract paranthesized subgroups
+    if(string[i] === '(') {
+      const j = string.lastIndexOf(')')
+      // console.log(`subgroup: ${string.substring(i+1, j)}`)
+      tokens.push(tokenise(string.substring(i+1, j)))
+      i += j-i+1
+    }
     // Try for identifier
     substr = string.substr(i)
-    matchData = substr.match(/(^[a-zA-Z]+)/)
+    matchData = substr.match(identifierRegex)
     if (matchData) {
+      // console.log(`word: ${matchData[0]}`)
       matchData[0].length > 0 && tokens.push(matchData[0])
       i += matchData[0].length
     }
     // Try for operator
     substr = string.substr(i)
-    matchData = substr.match(/^([!#$%&*+\-\/<=>@^_.,;]+)/)
+    matchData = substr.match(operatorRegex)
     if (matchData) {
+      // console.log(`operator: ${matchData[0]}`)
       matchData[0].length > 0 && tokens.push(matchData[0])
-      i += matchData[0].length
-    }
-    // Skip over whitespace
-    substr = string.substr(i)
-    matchData = substr.match(/^(\s)+/)
-    if(matchData) {
       i += matchData[0].length
     }
   }
@@ -40,9 +49,25 @@ function tokenise(string) {
 
 // console.log(tokenise("")) // []
 // console.log(tokenise("()")) // [ [] ]
-// console.log(tokenise("a (Word)")) // [ "a", [ "Word" ] ]
-console.log(tokenise("These are tokens.")) // [ "These", "are", "tokens", "." ]
+// console.log(tokenise("a (Word) foo")) // [ "a", [ "Word" ] ]
+// console.log(tokenise("These are tokens.")) // 
+// console.log(JSON.stringify(tokenise("These are tokens.")))
+// [ "These", "are", "tokens", "." ]
+// console.log(0===JSON.stringify(tokenise("These are tokens.")).localeCompare(`["These","are","tokens","."]`))
 // console.log(tokenise("add(a, b) = a + b")) // [ "add", [ "a", ",", "b" ], "=", "a", "+", "b" ]
 // console.log(tokenise("A *(B ^& C)")) // [ "A", "*", [ "B", "^&", "C" ] ]
 // console.log(tokenise("do $ readLine >>= putStrLn")) // [ "do", "$", "readLine", ">>=", "putStrLn" ]
 // console.log(tokenise("A single mismatched bracket (")) // null
+
+describe("Simple Syntax Tokenizing", ()=>{
+  it("Example Tests", ()=>{
+    Test.assertDeepEquals( tokenise(""), [] );
+    Test.assertDeepEquals( tokenise("()"), [ [] ] );
+    Test.assertDeepEquals( tokenise("a (Word)"), [ "a", [ "Word" ] ] );
+    Test.assertDeepEquals( tokenise("These are tokens."), [ "These", "are", "tokens", "." ] );
+    Test.assertDeepEquals( tokenise("add(a, b) = a + b"), [ "add", [ "a", ",", "b" ], "=", "a", "+", "b" ] );
+    Test.assertDeepEquals( tokenise("A *(B ^& C)"), [ "A", "*", [ "B", "^&", "C" ] ] );
+    Test.assertDeepEquals( tokenise("do $ readLine >>= putStrLn"), [ "do", "$", "readLine", ">>=", "putStrLn" ] );
+    Test.assertDeepEquals( tokenise("A single mismatched bracket ("), null );
+  })
+})
