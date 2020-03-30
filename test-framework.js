08b0afdfa3c31c793b6edbb40f94f4c81f595803
Scott <scott.bishop.dev@gmail.com>
2020-03-29 20:51:22 -0700

    Fix test helper's recursion

--- test-framework.js
+++ test-framework.js
@@ -1,20 +1,21 @@
 
-const Test = {
-  assertDeepEquals: (returnVal, expectVal) => {
-    if (returnVal === expectVal) {return true}
-    if (Array.isArray(returnVal !== Array.isArray(expectVal)) ||
-      returnVal === null || returnVal === undefined ||
-      expectVal === null || expectVal === undefined ||
-      Object.keys(returnVal).length !== Object.keys(expectVal).length) {
-        console.error('assertDeepEquals is false, these are not the same:')    
-        console.error(`  ${JSON.stringify(returnVal)}\n  ${JSON.stringify(expectVal)}`)
-        return false
-    }
-    return Object.keys(returnVal).every(key => assertDeepEquals(returnVal[key], expectVal[key]))
-  },
+assertDeepEquals = (returnVal, expectVal) => {
+  if (returnVal === expectVal) {return true}
+  if (Array.isArray(returnVal !== Array.isArray(expectVal)) ||
+    returnVal === null || returnVal === undefined ||
+    expectVal === null || expectVal === undefined ||
+    Object.keys(returnVal).length !== Object.keys(expectVal).length) {
+      console.error('assertDeepEquals is false, these are not the same:')    
+      console.error(`  ${JSON.stringify(returnVal)}\n  ${JSON.stringify(expectVal)}`)
+      return false
+  }
+  return Object.keys(returnVal).every(key => assertDeepEquals(returnVal[key], expectVal[key]))
 }
 
-console.log(Test)
+// Types of test available
+const Test = {
+  assertDeepEquals,
+}
 
 // 'describe' and 'it' just run whatever they're given
 describe = it = (a, b) => b()
