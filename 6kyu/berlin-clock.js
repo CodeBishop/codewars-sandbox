58b273445f0f7556c1c5be7b999b4c8ee8f0ecb4
CodeBishop <scott.bishop.dev@gmail.com>
2020-08-14 21:10:17 -0700

    Add berlin clock

---
+++ 6kyu/berlin-clock.js
@@ -0,0 +1,14 @@
+function berlinClock(time) {
+  const out = ''
+  return "O7RROO7RROO7YYRYYRYYRYY7YOOO"
+}
+
+
+console.log(berlinClock("12:56:01"))
+console.log("O7RROO7RROO7YYRYYRYYRYY7YOOO\n")
+
+console.log(berlinClock("00:00:00"))
+console.log("Y7OOOO7OOOO7OOOOOOOOOOO7OOOO\n")
+
+console.log(berlinClock("22:32:45"))
+console.log("O7RRRR7RROO7YYRYYROOOOO7YYOO\n")
