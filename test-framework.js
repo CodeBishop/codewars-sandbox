958a567c9060d1b512cc9c79d8d594dc3d7957a2
CodeBishop <scott.bishop.dev@gmail.com>
2021-01-22 02:14:00 -0800

    Create an assertSimilar() testing function

--- test-framework.js
+++ test-framework.js
@@ -25,6 +25,19 @@ const assertEquals = (a, b, msg) => {
   }
 }
 
+const assertSimilar = (returnVal, expectVal) => {
+  if (returnVal == expectVal) {return true}
+  if (Array.isArray(returnVal != Array.isArray(expectVal)) ||
+    returnVal == null || returnVal == undefined ||
+    expectVal == null || expectVal == undefined ||
+    Object.keys(returnVal).length != Object.keys(expectVal).length) {
+      console.error('assertSimilar is false, these are not the same:')    
+      console.error(`  ${JSON.stringify(returnVal)}\n  ${JSON.stringify(expectVal)}`)
+      return false
+  }
+  return Object.keys(returnVal).every(key => assertSimilar(returnVal[key], expectVal[key]))
+}
+
 const expectError = (msg, fn) => {
   console.log(msg)
   try {
@@ -48,10 +61,10 @@ const it = describe
 const Test = {
   assertDeepEquals,
   assertEquals,
+  assertSimilar,
   describe,
   expectError,
   it,
 }
-
 
 module.exports = {it, describe, Test}
